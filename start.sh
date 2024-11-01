f505541e9f28295f0d9de8905ba6f62702e9c23b
Ahmed Karem Mousa <59775002+AhmedMousa-ag@users.noreply.github.com>
2024-11-01 11:14:18 +0200

    feat(Docker): start backend service when init db completed

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@ ENV_FILE=./backend/rust-backend/.env
 
 # docker compose --env-file=./backend/rust-backend/.env up init-db --build 
 
-docker compose --env-file=$ENV_FILE up init-db --build 
-docker compose --env-file=$ENV_FILE up --build
+docker compose --env-file=$ENV_FILE up --build -d 
