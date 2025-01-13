1644cd22e7e355177fd8c4600c3b1639415a038b
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-01-13 21:19:57 +0200

    chore(backend, rust, diesel): modify for linux

--- backend/rust-backend/src/models/psql/schema.rs
+++ backend/rust-backend/src/models/psql/schema.rs
@@ -1,6 +1,59 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    course (id) {
+        id -> Int8,
+        #[max_length = 255]
+        name -> Nullable<Varchar>,
+        #[max_length = 255]
+        title -> Nullable<Varchar>,
+        instructor_id -> Nullable<Int8>,
+        course_id -> Nullable<Int8>,
+    }
+}
+
+diesel::table! {
+    employee (id) {
+        id -> Int4,
+        #[max_length = 255]
+        email -> Nullable<Varchar>,
+        #[max_length = 255]
+        first_name -> Nullable<Varchar>,
+        #[max_length = 255]
+        last_name -> Nullable<Varchar>,
+    }
+}
+
+diesel::table! {
+    instructor (id) {
+        id -> Int8,
+        #[max_length = 255]
+        email_name -> Nullable<Varchar>,
+        #[max_length = 255]
+        first_name -> Nullable<Varchar>,
+        #[max_length = 255]
+        last_name -> Nullable<Varchar>,
+        instructor_detail_id -> Nullable<Int8>,
+    }
+}
+
+diesel::table! {
+    instructor_detail (id) {
+        id -> Int8,
+        #[max_length = 255]
+        hobby -> Nullable<Varchar>,
+        #[max_length = 255]
+        youtube_channel -> Nullable<Varchar>,
+    }
+}
+
+diesel::table! {
+    instructordetail (id) {
+        id -> Int8,
+    }
+}
+
+diesel::table! {
     posts (id) {
         id -> Int4,
         title -> Text,
@@ -9,6 +62,20 @@ diesel::table! {
         post_type -> Text,
         created_at -> Nullable<Timestamp>,
         edited_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    student (id) {
+        id -> Int4,
+        #[max_length = 255]
+        email -> Nullable<Varchar>,
+        #[max_length = 255]
+        first_name -> Nullable<Varchar>,
+        #[max_length = 255]
+        last_name -> Nullable<Varchar>,
+        #[max_length = 255]
+        new_column -> Nullable<Varchar>,
     }
 }
 
@@ -22,4 +89,15 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(posts, users,);
+diesel::joinable!(instructor -> instructor_detail (instructor_detail_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    course,
+    employee,
+    instructor,
+    instructor_detail,
+    instructordetail,
+    posts,
+    student,
+    users,
+);
